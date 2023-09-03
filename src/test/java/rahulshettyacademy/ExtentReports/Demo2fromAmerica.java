261919b7256ed9939b51cc99ca38b05580393827
Nisha Hashmi <nishahashmi9313@gmail.com>
2023-09-03 14:17:08 +0530

    2nd commit froM america

---
+++ src/test/java/rahulshettyacademy/ExtentReports/Demo2fromAmerica.java
@@ -0,0 +1,11 @@
+package rahulshettyacademy.ExtentReports;
+
+public class Demo2fromAmerica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i m demo2 from america");
+
+	}
+
+}
